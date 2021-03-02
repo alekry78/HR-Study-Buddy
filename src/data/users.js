629dd849f8e79a69fddf5c9d057dc832e2698e14
alekry78 <krysaleksander1@gmail.com>
2021-03-02 21:40:27 +0100

    users-list-component

---
+++ src/data/users.js
@@ -0,0 +1,37 @@
+export const users = [
+  {
+    name: 'Adam Romanski',
+    attendance: '38%',
+    average: '2.3',
+  },
+  {
+    name: 'Krzysztof Batko',
+    attendance: '46%',
+    average: '3.3',
+  },
+  {
+    name: 'Patrycja Gonciarz',
+    attendance: '48%',
+    average: '2.8',
+  },
+  {
+    name: 'Olga Hahn',
+    attendance: '78%',
+    average: '4.1',
+  },
+  {
+    name: 'Paweł Andrzejewski',
+    attendance: '10%',
+    average: '2.1',
+  },
+  {
+    name: 'Szymon Knuth',
+    attendance: '56%',
+    average: '3.3',
+  },
+  {
+    name: 'Krystian Foczpaniak',
+    attendance: '98%',
+    average: '4.9',
+  },
+];
